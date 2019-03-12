baaa0ef50deac270b3e452c211f15331b8378bf1
Hector <a01632340@itesm.mx>
2019-03-12 03:17:52 +0000

    Set priority y get priority

---
+++ lspriority.c
@@ -0,0 +1,16 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+int main(int argc,char **argv){
+	int pid;
+	int priority;
+	pid=atoi(argv[1]);
+	priority= getpriority(pid);
+	if(priority==-1){
+		printf(1,"Process no found with id %d \n",pid);
+	}
+	else{
+		printf(1,"Process %d has priority %d \n",pid,priority);
+	}
+	exit();
+}
